38654ee20c66d5e1990c88ce16f8bd3069179b0d
JoshiRL2099 <apato_2003@yahoo.com.mx>
2026-04-30 15:02:17 -0600

    add ssh conection to aws instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,6 +115,8 @@ resource "aws_instance" "web_server" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
+  key_name = "lms-key"
+
   tags = {
     Name = "lms-server"
   }
